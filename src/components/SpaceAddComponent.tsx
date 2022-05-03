4b9d48727bb295db528d0ac47e2adedb5c109073
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-03 20:35:59 +0630

    update: init

---
+++ src/components/SpaceAddComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SpaceAddComponent() {
+  return (
+    <div
+      className="space-add-component"
+      onClick={() => {
+        // alert("Lwin");
+      }}
+    >
+      Space Add Component
+    </div>
+  );
+}
---
+++ src/components/Switcher/Swithcer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Swithcer() {
+  return (
+    <>
+      <a
+        href="#"
+        className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500"
+      >
+        <div className="items-center space-y-3">
+          <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">
+            New project
+          </h3>
+          <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">
+            New project
+          </h3>
+          <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">
+            New project
+          </h3>
+        </div>
+        <p className="text-slate-500 group-hover:text-white text-sm">
+          Create a new project from a variety of starting templates.
+        </p>
+      </a>
+    </>
+  );
+}
+
+export default Swithcer;
